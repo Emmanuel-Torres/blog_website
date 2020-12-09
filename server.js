72214a4733b3480e9c2e30bb4e7e3b76829c06bb
Ofnir01 <emmanuel.torresmaya@gmail.com>
2020-12-09 10:16:10 -0700

    added style to the header of the index page

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const port = 3000;
 const Blog = require('./Models/Blog.js');
 
-let blogs = [new Blog('Test Blog', 'Test Desc', 'Test Posted on Date')];
+let blogs = [new Blog('Test Blog', 'Test Description', 'Test Category', '01/01/1961')];
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
@@ -34,6 +34,10 @@ app.post('/addblog', (req, res) => {
     console.log('Posted Succesfully added');
     res.redirect(301, '/');
 });
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname+'/Pages/about.html'));
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
